006c19c5503a7049b97b6ae447a07ebaa74348f1
Jilme <73157441+Jilme@users.noreply.github.com>
2020-10-20 08:15:35 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
-music.playMelody("C5 - - F - - - - ", 120)
+input.onButtonPressed(Button.A, function () {
+    music.playMelody("C5 - - F - - - - ", 120)
+})
 basic.forever(function () {
     led.plot(1, 4)
 })
